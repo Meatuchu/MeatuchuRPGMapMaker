791a431ec7afe01361832631dacfb80b79df7f09
Meatuchu <pippee357@gmail.com>
2024-04-11 22:29:30 -0400

    feat: KeyState class will only trigger once per match unless otherwise specified

--- MeatuchuRPGMapMaker/keybinds/KeyState.py
+++ MeatuchuRPGMapMaker/keybinds/KeyState.py
@@ -6,12 +6,17 @@ from MeatuchuRPGMapMaker.constants import NS_PER_MS
 
 
 class KeyState:
-    def __init__(self, keys: Dict[str, int]) -> None:
+    allow_hold_refire: bool
+    __fired: bool
+
+    def __init__(self, keys: Dict[str, int], allow_hold_refire: bool = False) -> None:
         for key in keys:
             if keys[key] < 0:
                 raise ValueError("Keybinds must have a hold requirement of at least 0 ns")
 
+        self.allow_hold_refire = allow_hold_refire
         self.keys = keys
+        self.__fired = False
 
     def check(self, input_snapshot: InputSnapshot) -> bool:
         for key, hold_requirement in self.keys.items():
@@ -19,5 +24,11 @@ class KeyState:
                 input_snapshot.keys.get(key)
                 and input_snapshot.keys[key] + (hold_requirement * NS_PER_MS) < time.time_ns()
             ):
+                self.__fired = False
                 return False
+
+        if (not self.allow_hold_refire) and self.__fired:
+            return False
+
+        self.__fired = True
         return True
