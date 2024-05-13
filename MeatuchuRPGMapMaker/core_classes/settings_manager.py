9f0ad0febbe98bcd7bdf0dd9637337008f531954
Meatuchu <pippee357@gmail.com>
2024-05-13 13:58:15 -0400

    feat: SettingsManager has write for settings

--- MeatuchuRPGMapMaker/core_classes/settings_manager.py
+++ MeatuchuRPGMapMaker/core_classes/settings_manager.py
@@ -1,11 +1,15 @@
 # Global app settings
-from typing import Any
+from typing import Any, Dict, Literal
 
 from . import FeatureManager
 from .event_manager import EventManager
 
-__settings_values__ = {
-    "default": {
+STAGE_CONFIG_TYPE = Dict[str, Dict[str, Any]]
+GROUP_CONFIG_TYPE = Dict[str, Any]
+
+
+__settings_values__: Dict[Literal["any", "prod", "beta", "dev"], STAGE_CONFIG_TYPE] = {
+    "any": {
         "window": {"width": 1920, "height": 1080, "fullscreen_mode": 2, "name": "test"},
         "app": {
             "tickrate": 60,
@@ -35,20 +39,38 @@ settings_instance = 0
 
 class SettingsManager(FeatureManager):
     event_mgr: EventManager
-    session_settings = {}
+    session_settings: GROUP_CONFIG_TYPE = {}
 
     def __init__(self) -> None:
         super().__init__()
 
-    def get_setting(self, group: str, key: str) -> Any:
+    def get_setting(self, group: str, key: str, default: Any = None, allow_none: bool = True) -> Any:
+        session_value = self.session_settings.get(group, {}).get(key)
+        if session_value:
+            return session_value
+
+        fallback_value = __settings_values__["any"].get(group, {}).get(key, default)
+
         stage_config = __settings_values__.get(self.stage.value, {})
-        default = __settings_values__["default"]
-        v = stage_config.get(group, default[group]).get(key, default[group][key])
+        value = stage_config.get(group, {}).get(key, fallback_value)
+
         self.log(
             "DEBUG",
-            f'Retrieved value {f"{v}" if isinstance(v, str) else v} from setting "{key}" in group "{group}" for stage "{self.stage}"',
+            f'Retrieved value {f"{value}" if isinstance(value, str) else value} from setting "{key}" in group "{group}" for stage "{self.stage}"',
         )
-        return v
+
+        if value is None and not allow_none:
+            raise AttributeError(f"Setting {key} in group {group} not found")
+
+        return value
+
+    def set_setting(self, group: str, key: str, value: Any) -> None:
+        self.session_settings[group] = self.session_settings.get(group, {}) or {}
+        self.session_settings[group][key] = value
+        self.log(
+            "DEBUG",
+            f'Set value {f"{value}" if isinstance(value, str) else value} for setting "{key}" in group "{group}"',
+        )
 
     def register_event_manager(self, event_mgr: EventManager) -> None:
         self.event_mgr = event_mgr
--- tests/core_classes/test_settings_manager.py
+++ tests/core_classes/test_settings_manager.py
@@ -1,8 +1,10 @@
 from unittest.mock import MagicMock, patch
 
+from pytest import raises
+
 from MeatuchuRPGMapMaker.core_classes.settings_manager import SettingsManager
 
-mock_settings = {"default": {"window": {"width": 123, "height": 456}}}
+mock_settings = {"any": {"window": {"width": 123, "height": 456}}}
 
 
 def test_construction() -> None:
@@ -26,3 +28,39 @@ def test_register_event_manager() -> None:
     m.register_event_manager(event_mgr)
     m.event_mgr = event_mgr
     m.subscribe_to_events.assert_called_once()
+
+
+def test_settings_manager_sets_new_settings() -> None:
+    m = SettingsManager()
+    m.set_setting("window", "title", "test_title")
+    assert m.get_setting("window", "title") == "test_title"
+
+
+@patch(
+    "MeatuchuRPGMapMaker.core_classes.settings_manager.__settings_values__",
+    mock_settings,
+)
+def test_settings_manager_overrwrites_settings() -> None:
+    m = SettingsManager()
+    assert m.get_setting("window", "width") == 123
+    m.set_setting("window", "width", 456)
+    assert m.get_setting("window", "width") == 456
+
+
+@patch(
+    "MeatuchuRPGMapMaker.core_classes.settings_manager.__settings_values__",
+    mock_settings,
+)
+def test_settings_manager_missing_setting() -> None:
+    settings_instance = SettingsManager()
+    assert not settings_instance.get_setting("window", "made_up")
+
+
+@patch(
+    "MeatuchuRPGMapMaker.core_classes.settings_manager.__settings_values__",
+    mock_settings,
+)
+def test_settings_manager_missing_setting_not_allow_none() -> None:
+    settings_instance = SettingsManager()
+    with raises(AttributeError):
+        settings_instance.get_setting("window", "made_up", allow_none=False)
